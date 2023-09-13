fbac2f25999003fe38e0d73ccd2f725e99f883f7
rohit186201 <73100818+rohit186201@users.noreply.github.com>
2023-09-14 01:57:49 +0530

    Update code1.cpp

--- code1.cpp
+++ code1.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-string ArrayChallenge(string strArr[], int arrLength) {
+string ArrayChallenge(string strArr[], int arrLength, string ChallengeToken) {
     vector<string> cache;  // Initialize a vector to represent the cache
     const int maxCacheSize = 5;  // Maximum cache size
 
@@ -34,25 +34,25 @@ string ArrayChallenge(string strArr[], int arrLength) {
     }
     cacheState.pop_back();  // Remove the trailing hyphen
 
-    // Convert the cacheState to uppercase
-    transform(cacheState.begin(), cacheState.end(), cacheState.begin(), ::toupper);
-
-    // Remove characters present in ChallengeToken (case insensitive)
-    string ChallengeToken = "bz93vncjw71";
-    for (char c : ChallengeToken) {
-        cacheState.erase(remove(cacheState.begin(), cacheState.end(), toupper(c)), cacheState.end());
-    }
-
     // Reverse the cacheState string to get the correct order
     reverse(cacheState.begin(), cacheState.end());
+
+    // Concatenate the ChallengeToken to the cacheState
+    cacheState += ChallengeToken;
+
+    // Replace every third character with "X"
+    for (int i = 2; i < cacheState.length(); i += 3) {
+        cacheState[i] = 'X';
+    }
 
     return cacheState;  // Return the final cache state as a string
 }
 
 int main(void) {
     // Example input
-    string A[] = {"A", "B", "C", "D", "E", "D", "Q", "Z", "C"};
+    string A[] = {"A", "B", "A", "C", "A", "B"};
     int arrLength = sizeof(A) / sizeof(*A);
-    cout << ArrayChallenge(A, arrLength) << endl;
+    string ChallengeToken = "yzcksg10a4";
+    cout << ArrayChallenge(A, arrLength, ChallengeToken) << endl;
     return 0;
 }
